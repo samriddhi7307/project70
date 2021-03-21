52a1744854388fbdca0c079c7e2cc8745f26f3b2
samriddhi7307 <72978417+samriddhi7307@users.noreply.github.com>
2021-03-21 18:55:51 +0530

    Add files via upload

---
+++ screens/ReadStoryScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text,View} from 'react-native';
+
+export default class ReadStoryScreen extends React.Component{
+    render(){
+        return(
+            <View>
+                <Text style={{marginTop:400}}>
+                    BOOK
+                    </Text>
+                </View>
+        );
+    }
+}
---
+++ screens/WriteStoryScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text,View} from 'react-native';
+
+export default class WriteStoryScreen extends React.Component{
+    render(){
+        return(
+            <View>
+                <Text style={{marginTop:400}}>
+                    FACEBOOK
+                    </Text>
+                </View>
+        );
+    }
+}
